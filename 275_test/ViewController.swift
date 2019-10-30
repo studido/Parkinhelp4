883925faabfd09cce4530e25bff81767f47573d9
Sterling Smith <srs17@asb9700-e01.csil.sfu.ca>
2019-10-29 17:20:19 -0700

    initial commit

--- 275_test/ViewController.swift
+++ 275_test/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var WelcomeLabel: UILabel!
+    @IBOutlet weak var GoogleSignIn: UIButton!
+    @IBOutlet weak var FacebookSignIn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.FacebookSignIn.layer.cornerRadius = 10
+        self.GoogleSignIn.layer.cornerRadius = 10
+        self.WelcomeLabel.layer.cornerRadius = 10
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
 }
+
 
