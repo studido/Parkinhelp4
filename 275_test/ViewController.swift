0929a1dce4aa03e7c56bf681d6a5db9f6587addd
Evan Lee <evanlee@d207-023-197-165.wireless.sfu.ca>
2019-10-31 19:45:24 -0700

    fixed merging issues

--- 275_test/ViewController.swift
+++ 275_test/ViewController.swift
@@ -14,22 +14,25 @@ class ViewController: UIViewController, GIDSignInUIDelegate {
     @IBOutlet weak var WelcomeMessage: UILabel!
     @IBOutlet weak var ParkinHelpIcon: UIImageView!
     @IBOutlet weak var WelcomeLabel: UILabel!
-    @IBOutlet weak var GoogleSignIn: UIButton!
+    
     @IBOutlet weak var FacebookSignIn: UIButton!
     @IBOutlet weak var TeamDancingDragon: UILabel!
+    
+    @IBOutlet weak var gSignIn: GIDSignInButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.FacebookSignIn.layer.cornerRadius = 10
         FacebookSignIn.layer.borderColor = UIColor.blue.cgColor
         FacebookSignIn.layer.borderWidth = 1
-        self.GoogleSignIn.layer.cornerRadius = 10
-        GoogleSignIn.layer.borderWidth = 1
-        GoogleSignIn.layer.borderColor = UIColor.blue.cgColor
+        
         ParkinHelpIcon.layer.cornerRadius = 30
         ParkinHelpIcon.layer.borderColor = UIColor.blue.cgColor
         ParkinHelpIcon.layer.borderWidth = 4
         WelcomeLabel.layer.cornerRadius = 30
+        
     
         // Do any additional setup after loading the view, typically from a nib.
         GIDSignIn.sharedInstance()?.uiDelegate = self
@@ -40,7 +43,6 @@ class ViewController: UIViewController, GIDSignInUIDelegate {
         let gSignIn = GIDSignInButton(frame: CGRect(x: 0, y:0, width: 230, height:48))
         gSignIn.center = view.center
         view.addSubview(gSignIn)
-        
     }
 
 
