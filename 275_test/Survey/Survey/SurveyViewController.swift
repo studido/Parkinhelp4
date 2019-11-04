dc9c595a876992dbf9131b1f83980ddd986d9baf
Evan Lee <evanlee@Evans-MacBook-Pro-2.local>
2019-11-03 21:14:45 -0800

    Add survey

---
+++ 275_test/Survey/Survey/SurveyViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Survey
+//
+//  Created by Liangchen Zou on 11/1/19.
+//  Copyright © 2019 Liangchen Zou/Sachin. All rights reserved.
+//
+
+import UIKit
+
+class SureyViewController: UIViewController {
+
+    //let physicalConditionData = ["Mild", "Intermediate", "Severe"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+
+}
+
