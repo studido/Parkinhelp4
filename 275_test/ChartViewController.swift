53b6f4801b8056bd3a078f0753be072318b39311
MICHAEL <michael@d142-058-034-206.burnaby.sfu.ca>
2019-11-17 16:03:49 -0800

    Modifyied statistic chart, connected to the firebase

--- 275_test/ChartViewController.swift
+++ 275_test/ChartViewController.swift
@@ -1,62 +1,49 @@
 //
 //  ViewController.swift
-//  BarChartLiveStream
+//  BarChart
 //
-//  Created by Brian Voong on 3/12/17.
-//  Copyright © 2017 Lets Build That App. All rights reserved.
+//  Created by Nguyen Vu Nhat Minh on 19/8/17.
+//  Copyright © 2017 Nguyen Vu Nhat Minh. All rights reserved.
 //
 
 import UIKit
 
-class ChartViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+class ChartViewController: UIViewController {
     
-    let cellId = "cellId"
-    
-    let values: [CGFloat] = [200, 300, 400, 500, 600, 700, 50, 20, 10, 5, 650, 2000, 3000, 500]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView?.backgroundColor = .white
-        
-        collectionView?.register(BarCell.self, forCellWithReuseIdentifier: cellId)
-        
-        (collectionView?.collectionViewLayout as? UICollectionViewFlowLayout)?.scrollDirection = .horizontal
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 4
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return values.count
-    }
-    
-    func maxHeight() -> CGFloat {
-        return view.frame.height - 20 - 44 - 8
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! BarCell
-        
-        if let max = values.max() {
-            let value = values[indexPath.item]
-            let ratio = value / max
+        let chart = BasicBarChart(frame: CGRect(x: 0, y: 160, width: 500, height: 200))
+        let dataEntries = [
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title"),
+            DataEntry(color: UIColor.red, height: 0.2, textValue: "20", title: "Some title"),
+            DataEntry(color: UIColor.green, height: 0.7, textValue: "70", title: "Some title")
             
-            cell.barHeightConstraint?.constant = maxHeight() * ratio
-        }
+        ]
+        chart.updateDataEntries(dataEntries: dataEntries, animated: true)
+        self.view.addSubview(chart) // Assuming that you are writing this code inside a view controller
         
-        return cell
     }
     
     
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 30, height: maxHeight())
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 4, bottom: 0, right: 4)
-    }
-    
+
 }
 
