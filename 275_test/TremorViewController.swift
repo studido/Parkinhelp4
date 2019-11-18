a1db4a66aca83b5022071ddc54a1a5b43518f030
Evan Lee <evanlee@d207-023-194-065.wireless.sfu.ca>
2019-11-18 15:25:04 -0800

    Added go to home button in tremor over view controller

--- 275_test/TremorViewController.swift
+++ 275_test/TremorViewController.swift
@@ -30,6 +30,7 @@ class TremorViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
         print("Tremor view loaded")
         Whiteback.layer.cornerRadius = 15
         Whiteback.layer.borderColor = UIColor.black.cgColor
