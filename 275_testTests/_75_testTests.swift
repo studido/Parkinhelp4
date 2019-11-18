19c12268832fcc794bdfbbe509af1482742b38db
Evan Lee <evanlee@d207-023-194-065.wireless.sfu.ca>
2019-11-18 15:18:38 -0800

    Added unit tests and fixed tremor back button issue

--- 275_testTests/_75_testTests.swift
+++ 275_testTests/_75_testTests.swift
@@ -7,10 +7,16 @@
 //
 
 import XCTest
+import Firebase
+import FirebaseDatabase
 @testable import _75_test
 
 
 class _75_testTests: XCTestCase {
+    override class func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        FirebaseApp.configure()
+    }
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -20,9 +26,60 @@ class _75_testTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testReadAndWriteToDatabase() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let firstName = "john"
+        let lastName = "doe"
+        let weight = "170"
+        let age = "79"
+        let height = "180"
+        let gender = "male"
+        let dateOfBirth = "12-10/1855"
+        let contactNumber = "6042339505"
+        let durationOfDisease = "41"
+        let repetitions = "2"
+        let intensity = "3"
+        let email = "johndoe@gmail.com"
+        let userType = "patient"
+        
+        let ref : DatabaseReference! = Database.database().reference()
+        let userData = ["firstName": firstName,
+                        "lastName":  lastName,
+                        "weight":  weight,
+                        "age":  age,
+                        "height":  height,
+                        "gender":  gender,
+                        "dateOfBirth":  dateOfBirth,
+                        "contactNumber":  contactNumber,
+                        "durationOfDisease":  durationOfDisease,
+                        "repetitions":  repetitions,
+                        "intensity":  intensity,
+                        "email" : email,
+                        "userType": userType]
+        
+        let uid = "dummy_uid"
+        ref.updateChildValues(["/Users/\(uid)" : userData])
+        
+    ref.child("Users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
+        XCTAssertTrue(snapshot.exists())
+        let value = snapshot.value as! [String: Any]
+        XCTAssertEqual(firstName, value["firstName"] as? String ?? "")
+        XCTAssertEqual(lastName, value["lastName"] as? String ?? "")
+        XCTAssertEqual(age, value["age"] as? String ?? "")
+        XCTAssertEqual(weight, value["weight"] as? String ?? "")
+        XCTAssertEqual(height, value["height"] as? String ?? "")
+        XCTAssertEqual(gender, value["gender"] as? String ?? "")
+        XCTAssertEqual(contactNumber, value["contactNumber"] as? String ?? "")
+        XCTAssertEqual(durationOfDisease, value["durationOfDisease"] as? String ?? "")
+        XCTAssertEqual(email, value["email"] as? String ?? "")
+        XCTAssertEqual(userType, value["userType"] as? String ?? "")
+        XCTAssertEqual(dateOfBirth, value["dateOfBirth"] as? String ?? "")
+        XCTAssertEqual(repetitions, value["repetitions"] as? String ?? "")
+        XCTAssertEqual(intensity, value["intensity"] as? String ?? "")
+        
+        })
     }
 
     func testPerformanceExample() {
