3ab5209591884caa2cbd03e6a2d61f9dfade62bb
Evan Lee <evanlee@d207-023-160-130.wireless.sfu.ca>
2019-11-25 19:59:39 -0800

    Added method for doctor to add patients

--- 275_test/ChartViewController.swift
+++ 275_test/ChartViewController.swift
@@ -49,7 +49,6 @@ class ChartViewController: UIViewController {
     }})
         
         return dateAndTremorScore
-
     }
 }
 
--- 275_test/DocPatientListViewController.swift
+++ 275_test/DocPatientListViewController.swift
@@ -15,6 +15,95 @@ class DocPatientListViewController: UIViewController {
 
     @IBOutlet weak var DocAcc: UIButton!
     
+    @IBOutlet weak var AddPatient: UIButton!
+    
+    @IBAction func addPatient(_sender: UIButton) {
+        let alertController = UIAlertController(title: "Add Patient", message: "", preferredStyle: .alert)
+        
+        alertController.addTextField { (textField : UITextField!) -> Void in
+            textField.placeholder = "Enter patient's email"
+        }
+        
+        let addButton = UIAlertAction(title: "Add", style: .destructive, handler: {(alert: UIAlertAction!) in
+            let textField = alertController.textFields![0]
+            let email = textField.text
+            let uid = Firebase.Auth.auth().currentUser!.uid
+            var emailsList : [String: String]!
+            if email != "" && email! != Variables.email {
+                let hashedEmail = getSha256(string: email!)
+                let ref : DatabaseReference = Database.database().reference()
+                //Check if the provided email exists in databse
+                ref.child("Emails").child(hashedEmail).observeSingleEvent(of: .value, with: { (snapshot) in
+                    if (snapshot.exists()) {
+                        let val = snapshot.value as! [String: String]
+                        let patientUID = val["uid"]!
+                        //Get the list of patients linked with doctor
+                        ref.child("Users").child(uid).child("patients").observeSingleEvent(of: .value, with: { (dataSnapshot) in
+                            if (dataSnapshot.exists()) {
+                                emailsList = (dataSnapshot.value as! [String: String])
+                                if let _ = emailsList[patientUID] {
+                                    //Doctor is already connected with this patient
+                                    let existsAlert = UIAlertController(title: "Patient already added", message: "", preferredStyle: .alert)
+                                    let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                                    existsAlert.addAction(okButton)
+                                    self.present(existsAlert, animated: true, completion: nil)
+                                    return
+                                }
+                                else {
+                                    //Add new patient to doctors patient list
+                                    emailsList[patientUID] = email!
+                                }
+
+                            }
+                            print("email entered is ", email!)
+                            emailsList = [patientUID : email!]
+                             ref.updateChildValues(["/Users/\(uid)/patients" : emailsList])
+                            
+                            //update patients doctor info
+                            //HERE
+                            
+                            
+                            let successAlert = UIAlertController(title: "Successfully added patient", message: "", preferredStyle: .alert)
+                            let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                            successAlert.addAction(okButton)
+                            self.present(successAlert, animated: true, completion: nil)
+                            
+                        })
+                    }
+                    else {
+                        //User entered an unregistered email
+                        print("entered unregistered email")
+                         let unregisteredEmailAlert = UIAlertController(title: "Unregistered email", message: "", preferredStyle: .alert)
+                        let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                        unregisteredEmailAlert.addAction(okButton)
+                        self.present(unregisteredEmailAlert, animated: true, completion: nil)
+                        
+                    }
+                    
+                })
+                
+            } else {
+                //User entered an empty stirng or its own email
+                let emptyEmailAlert = UIAlertController(title: "Please enter a valid email address", message: "", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                emptyEmailAlert.addAction(okButton)
+                self.present(emptyEmailAlert, animated: true, completion: nil)
+            }
+            
+        })
+        
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: {(alert: UIAlertAction!) in
+            print("cancel button pressed")
+            
+        })
+        
+        alertController.addAction(addButton)
+        alertController.addAction(cancelButton)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
     @IBAction func signOutUserLoggedInWithGoogle(_sender: UIButton) {
         GIDSignIn.sharedInstance()?.signOut()
         UserDefaults.standard.set(false, forKey: "usersignedIn")
@@ -27,6 +116,8 @@ class DocPatientListViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+
     }
     
 
