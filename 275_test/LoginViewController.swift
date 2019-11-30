370284c33f1d97bfa17be7fda7e2d6a6e7743828
Evan Lee <evanlee@d209-087-056-019.wireless.sfu.ca>
2019-11-29 16:28:29 -0800

    Display spinning wheel while loading/reading data and output tremor score

--- 275_test/LoginViewController.swift
+++ 275_test/LoginViewController.swift
@@ -15,7 +15,7 @@ import FirebaseDatabase
 
 class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     fileprivate var loggedIn = false
-    fileprivate let elapsed : Int = 30// 86400 * 7 //24 hours in seconds times 7 (1 week)
+    fileprivate let elapsed : Int = 86400 * 7 //24 hours in seconds times 7 (1 week)
     
     @IBOutlet weak var Background: UIImageView!
     
@@ -117,3 +117,30 @@ class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
     }
 }
 
+
+//Extension function for displaying/removing spin wheel during loading/writing data
+var vSpinner : UIView?
+
+extension UIViewController {
+    func showSpinner(onView : UIView) {
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        let ai = UIActivityIndicatorView.init(style: .whiteLarge)
+        ai.startAnimating()
+        ai.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(ai)
+            onView.addSubview(spinnerView)
+        }
+        
+        vSpinner = spinnerView
+    }
+    
+    func removeSpinner() {
+        DispatchQueue.main.async {
+            vSpinner?.removeFromSuperview()
+            vSpinner = nil
+        }
+    }
+}
